b025d769234034513c3acbc121e8f9d4634b741f
Timothy Dillman <timothy.dillman@mobilefirst.me>
2020-11-22 13:34:29 -0600

    Update transformable protocol to be public

--- Sources/PorkChop/Data+Extension.swift
+++ Sources/PorkChop/Data+Extension.swift
@@ -1,21 +1,21 @@
 import Foundation
 // FIXME: Will probably remove these extensions.  I don't like how they are currently and will further think about how best to
 // implement a class / struct that can assist in transforming data into the formats we need.
-protocol Transformable {
+public protocol Transformable {
     func transforming<T: Decodable>(type: T.Type) throws -> T
     func tryTransform<T: Decodable>(type: T.Type) -> T?
 }
 
-protocol EncodableData {
+public protocol EncodableData {
     func encode() -> Data?
 }
 
 extension Data: Transformable {
-    func transforming<T>(type: T.Type) throws -> T where T : Decodable {
+    public func transforming<T>(type: T.Type) throws -> T where T : Decodable {
         return try JSONDecoder().decode(T.self, from: self)
     }
     
-    func tryTransform<T>(type: T.Type) -> T? where T : Decodable {
+    public func tryTransform<T>(type: T.Type) -> T? where T : Decodable {
         return try? JSONDecoder().decode(T.self, from: self)
     }
 }
@@ -24,7 +24,7 @@ extension Encodable {
     /**
      Shorthand access providing an extension to any object that conforms to Encodable.
      */
-    func encode() -> Data? {
+    public func encode() -> Data? {
         return try? JSONEncoder().encode(self)
     }
 }
