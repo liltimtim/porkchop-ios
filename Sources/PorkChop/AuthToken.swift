e1b3ae847220536b27107f81b70a183c64a783ab
Timothy Dillman <timothy.dillman@mobilefirst.me>
2020-11-22 13:09:27 -0600

    make initial public methods.

--- Sources/PorkChop/AuthToken.swift
+++ Sources/PorkChop/AuthToken.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol PRKChopAuthToken: Encodable, Decodable {
+public protocol PRKChopAuthToken: Encodable, Decodable {
     var token: String { get set }
     var tokenType: String { get set }
     var expirationDate: String { get set }
@@ -15,24 +15,24 @@ protocol PRKChopAuthToken: Encodable, Decodable {
 
 public struct PRCKChopDefaultAuthenticationToken: PRKChopAuthToken {
     /** Assumes an ISO8601 Date String */
-    var expirationDate: String
-    var token: String
-    var tokenType: String
+    public var expirationDate: String
+    public var token: String
+    public var tokenType: String
     /** Computes the proper HTTP Header for Authorization in the form of "Authorization" : <auth_type> <token> */
-    var headerToken: [String:String] { return ["Authorization": "\(tokenType) \(token)"] }
+    public var headerToken: [String:String] { return ["Authorization": "\(tokenType) \(token)"] }
     
     /**
      Determines if the token has exceeded the expiration date of the token lifespan. Compares the incoming date has not exceeded the expiration date.
      
      - Parameter date: Date object that will be compared against the expiration date property
      */
-    func isExpired(_ date: Date) -> Bool {
+    public func isExpired(_ date: Date) -> Bool {
         let dateFormatter = ISO8601DateFormatter()
         let expDate = dateFormatter.date(from: expirationDate)!
         return date > expDate
     }
     
-    func expDate() -> Date? {
+    public func expDate() -> Date? {
         return parseDate()
     }
     
--- Sources/PorkChop/EmptyBody.swift
+++ Sources/PorkChop/EmptyBody.swift
@@ -2,5 +2,7 @@ import Foundation
 /**
  A simple empty body. Used when a POST, PUT, or PATCH request is made that does not require a body of data to be sent.
  */
-struct PRKChopEmptyBody: Encodable { }
+public struct PRKChopEmptyBody: Encodable {
+    public init() { } 
+}
 
