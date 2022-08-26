9913e3d5d6f255e19cc2fb231f767d3b0549ece9
Timothy Dillman <timothy.dillman@mobilefirst.me>
2022-08-26 18:58:05 -0500

    updated tvOS supported minimum version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [
         .iOS(SupportedPlatform.IOSVersion.v13),
         .macOS(SupportedPlatform.MacOSVersion.v10_15),
-        .watchOS(SupportedPlatform.WatchOSVersion.v7)
+        .watchOS(SupportedPlatform.WatchOSVersion.v7),
+        .tvOS(SupportedPlatform.TVOSVersion.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
